8b35fd1d266476c578cda8a6c00f76ad91e589bc
Emi1699 <emanuelbuliga16@gmail.com>
2023-03-02 21:07:20 +0200

    managed to parse audio file and transform it to .txt file

--- openAI/Fed Speech Recognition/main.py
+++ openAI/Fed Speech Recognition/main.py
@@ -5,5 +5,12 @@ from pytube import YouTube
 
 model = whisper.load_model('base')
 
-print(env.CHAT_GPT_KEY)
+output = model.transcribe("fed_meeting.mp4")
 
+
+f = open("output.txt", 'w')
+f.write(output['text'])
+f.close()
+
+f = open('output.txt', 'r')
+print(f.read())
